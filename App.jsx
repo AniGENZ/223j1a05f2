093872b2e75a938d178cb760f50017672b431b52
Anirudh <166196779+AniGENZ@users.noreply.github.com>
2025-08-05 11:46:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>AffordMed</h1>
+        <ul>
+          <li>service</li>
+          <li>about us</li>
+          
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
